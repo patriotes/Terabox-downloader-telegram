d3d05acda3e67325381b9d122970f54868d7491c
patriotes <154737887+patriotes@users.noreply.github.com>
2024-01-29 10:46:27 +0100

    Update api.js

--- api.js
+++ api.js
@@ -20,11 +20,23 @@ async function getDownloadLink(data) {
             }
         };
 
-        const response = await axios.post(
-            "https://terabox-dl.qtcloud.workers.dev/api/get-download",
-            data,
-            config
-        );
+        const options = {
+            method: 'GET',
+            url: 'https://terabox-downloader.p.rapidapi.com/',
+            params: { url: '<REQUIRED>' },
+            headers: {
+                'X-RapidAPI-Key': '4f2bd0fc83msh5ff6c418f6b6c71p1a366cjsn1e9ed473356a',
+                'X-RapidAPI-Host': 'terabox-downloader.p.rapidapi.com'
+            }
+        };
+
+        try {
+            const response = await axios.request(options);
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+        
         return response.data;
     } catch (error) {
         throw error;
